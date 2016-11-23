467c8509b757af31c2d9b2f8eab284dbd129022d
fran <fran@FranLinux.Home>
2016-11-23 21:34:33 +0100

    Se han añadido estados en la pantalla de lobby, y con esto se han solucionado varios bugs. Como multiples retos o retos a jugadores desconectados.

--- routes/index.js
+++ routes/index.js
@@ -11,29 +11,46 @@ router.get('/', usersRouter.ensureAuthenticated, function(req, res, next) {
 module.exports = router;
 
 module.exports.socketio = function(io) {
-    var users = [];
+    //hash map que guarda el nombre del usuario y el estado del jugador
+    var users = {};
+    //hash map para guardar los ids de cada usuarios
     var mapUsers = {};
+    //hash map para los retos pendientes
+    var challenge = {};
     //namespace
     var nsp = io.of('/home');
     
     nsp.on('connection', function(socket){
         //añadirlo al array de usuarios
-        users.push(socket.request.user);
+        users[socket.request.user]='listo';
         //Obtener el id
         mapUsers[socket.request.user] = socket.id;
         //mandar la lista de usuarios
         nsp.emit('usersList', users);
         
         socket.on('disconnect', function(){
+            //resetear el estado del usuario que espera una respuesta
+            users[challenge[socket.request.user]]='listo';
+            nsp.to(mapUsers[challenge[socket.request.user]]).emit('resetState');
             //borrar al usuario
-            users.splice(users.indexOf(socket.request.user), 1);
+            delete users[socket.request.user];
             delete mapUsers[socket.request.user];
+            delete challenge[challenge[socket.request.user]];
+            delete challenge[socket.request.user];
             //mandar la lista de usuarios
             nsp.emit('usersList', users);
-            //resetear cosas del cliente
         });
         
         socket.on('challenge', function(user){
+            //cambiar los estados
+            users[socket.request.user]='esperando';
+            users[user]='retado';
+            //añdir en retos pendientes
+            challenge[socket.request.user]=user;
+            challenge[user]=socket.request.user;
+            //actualizar la lista de los usuarios
+            nsp.emit('usersList', users);            
+            //mandar el reto
             nsp.to(mapUsers[user]).emit('challenge', socket.request.user);
         });
         
@@ -41,8 +58,15 @@ module.exports.socketio = function(io) {
             if(json.accepted){
                 nsp.to(mapUsers[json.user]).emit('responseChallenge', {rival: socket.request.user, accepted: true});
             }else{
+                //cambiar los estados
+                users[socket.request.user]='listo';
+                users[json.user]='listo';
+                //actualizar la lista de los usuarios
+                nsp.emit('usersList', users);
+                //enviar la respuesta
                 nsp.to(mapUsers[json.user]).emit('responseChallenge', {accepted: false});
             }
+            //eliminar de retos pendientes
         });
         
         socket.on('statistics', function(player){
