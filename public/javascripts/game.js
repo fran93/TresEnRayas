7356bc584b44529b25068ff2c508705145666058
fran <fran@FranLinux.Home>
2016-11-23 11:51:40 +0100

    Añadido botón para salir de las partidas, se ha corregido la interfaz cuando juegas contra el bot.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -34,13 +34,13 @@ $( document ).ready(function() {
         var selector = "";
         switch(pos){
             case 0: case 1: case 2:
-                selector = $('.row:eq(0) .square:eq('+pos+')');
+                selector = $('.row:eq(1) .square:eq('+pos+')');
             break;
             case 3: case 4: case 5:
-                 selector = $('.row:eq(1) .square:eq('+(pos-3)+')');
+                 selector = $('.row:eq(2) .square:eq('+(pos-3)+')');
             break;
             case 6: case 7: case 8:
-                 selector = $('.row:eq(2) .square:eq('+(pos-6)+')');
+                 selector = $('.row:eq(3) .square:eq('+(pos-6)+')');
             break;
         };
         $(selector).append("<div class='circle'> </div>");
